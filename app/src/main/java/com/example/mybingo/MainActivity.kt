e6336c801d3c50bc5fe7a5fab392a42cfe0e85b3
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-28 00:24:10 +0900

    add const and number override by editing

--- app/src/main/java/com/example/mybingo/MainActivity.kt
+++ app/src/main/java/com/example/mybingo/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.mybingo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var maxNumber: Int = 75
+    private lateinit var maxNumberEditText: EditText
+    private lateinit var registerMaxNumberButton: Button
+
+
+//    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        maxNumberEditText = findViewById(R.id.maxNumber)
+        registerMaxNumberButton = findViewById(R.id.registerMaxNumber)
+        registerMaxNumberButton.setOnClickListener(this)
+
+        maxNumberEditText.setText("${this.maxNumber}")
+    }
+
+    override fun onClick(v: View?) {
+        val maxNumberString = this.maxNumberEditText.text.toString()
+        maxNumber = maxNumberString.toInt()
+
+        Log.d("MainActivity", "maxNumber: $maxNumber")
+
+
+
     }
 }
